80351c5d683d870492ae49a3cea46bb956ee6f19
n1tr0n__ <166323600+n1tr0nR@users.noreply.github.com>
2024-11-21 09:41:27 +0000

    Initial commit

---
+++ src/main/java/com/nitron/reign_no_longer/ReignNoLongerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nitron.reign_no_longer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ReignNoLongerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
